6e60a50f9ed79a720a5edf3992ccf15dd00083ec
Jorge I. Meza <jimezam@autonoma.edu.co>
2021-02-02 22:06:53 +0000

    Se creó la entidad Link

---
+++ database/migrations/2021_02_02_215556_create_links_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('label', 128);            
+            $table->string('url', 256);
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
